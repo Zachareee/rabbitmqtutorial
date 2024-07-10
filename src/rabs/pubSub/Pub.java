74e8632d235053e98ebd7f05be659e2058dcbd11
Zachareee <zacharytanhx@yahoo.com>
2024-07-10 12:19:39 +0800

    PubSub tutorial complete

--- src/rabs/pubSub/Pub.java
+++ src/rabs/pubSub/Pub.java
@@ -1,6 +1,7 @@
 package rabs.pubSub;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 import com.rabbitmq.client.Channel;
@@ -12,13 +13,18 @@ import rabs.*;
 public class Pub {
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(null);
+        factory.setHost(VariableProvider.getVariable(EnvMap.hostname));
         String exchangeName = VariableProvider.getVariable(EnvMap.exchangeName);
 
         try (Connection conn = factory.newConnection();
-                Channel channel = conn.createChannel()) {
+                Channel channel = conn.createChannel();
+                Scanner sc = new Scanner(System.in)) {
             channel.exchangeDeclare(exchangeName, "fanout");
-            channel.basicPublish(exchangeName, "", null, "Hi".getBytes());
+
+            System.out.println("Type your message: ");
+            String msg = sc.nextLine();
+            channel.basicPublish(exchangeName, "", null, msg.getBytes());
+            System.out.println(" [x] Sent '" + msg + "'");
         }
     }
 }
--- src/rabs/pubSub/Sub.java
+++ src/rabs/pubSub/Sub.java
@@ -1,16 +1,20 @@
 package rabs.pubSub;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 import rabs.*;
 
 public class Sub {
     public static void main(String[] args) throws IOException, TimeoutException {
+        System.out.println(args[0]);
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(VariableProvider.getVariable(EnvMap.hostname));
         String exchangeName = VariableProvider.getVariable(EnvMap.exchangeName);
@@ -18,7 +22,32 @@ public class Sub {
         Connection conn = factory.newConnection();
         Channel channel = conn.createChannel();
 
+        channel.exchangeDeclare(exchangeName, "fanout");
         String queueName = channel.queueDeclare().getQueue();
         channel.queueBind(queueName, exchangeName, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            logMethod(args[0]).accept(new String(delivery.getBody()));
+        };
+        channel.basicConsume(queueName, true, callback, consumertag -> {
+        });
+    }
+
+    private static Consumer<String> logMethod(String method) {
+        return switch (method) {
+            case "print":
+                yield msg -> System.out.println(" [x] Message received: '" + msg + "'");
+            case "file":
+                yield Wrapper.wrap(msg -> {
+                    try (FileWriter writer = new FileWriter("logs.txt")) {
+                        writer.write(msg);
+                    }
+                    System.out.println(" [x] Message written: '" + msg + "'");
+                });
+            default:
+                yield null;
+        };
     }
 }
